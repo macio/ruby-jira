e4107033b0229886dab39ae865de115f78d2d911
Maciej Kozak <maciej.kozak@gmail.com>
2026-03-10 17:25:55 +0100

    Feat/projects api methods (#1)
    * feat: add update, delete, statuses, hierarchy, notification scheme project methods
    
    * test: replace hand-crafted fixtures with full Jira OpenAPI spec examples
    
    - project.json and projects.json now use official examples from dac-static.atlassian.com
    - fix missing @url tag on archive_project comment
    - update spec assertions to match new fixture values
    
    * docs: add release guide with versioning and git-cliff workflow

--- spec/jira/client/projects_spec.rb
+++ spec/jira/client/projects_spec.rb
@@ -11,28 +11,29 @@ RSpec.describe Jira::Client do
 
       expect(a_get("/project/search?status=live")).to have_been_made
       expect(projects).to be_a(Jira::PaginatedResponse)
-      expect(projects.total).to eq(2)
-      expect(projects.first[:key]).to eq("TEST")
-      expect(projects.last[:key]).to eq("DEMO")
+      expect(projects.total).to eq(7)
+      expect(projects.first[:key]).to eq("EX")
+      expect(projects.last[:key]).to eq("ABC")
       expect(projects.next_page?).to be(true)
     end
   end
 
   describe ".project" do
-    it "gets a single project" do
-      stub_get("/project/TEST", "project")
-      project = Jira.project("TEST")
+    it "gets a single project", :aggregate_failures do
+      stub_get("/project/EX", "project")
+      project = Jira.project("EX")
 
-      expect(a_get("/project/TEST")).to have_been_made
-      expect(project[:key]).to eq("TEST")
-      expect(project.dig(:lead, :displayName)).to eq("Test User")
+      expect(a_get("/project/EX")).to have_been_made
+      expect(project[:key]).to eq("EX")
+      expect(project[:name]).to eq("Example")
+      expect(project.dig(:lead, :displayName)).to eq("Mia Krystof")
     end
 
     it "passes query options" do
-      stub_get("/project/TEST?expand=description", "project")
-      Jira.project("TEST", expand: "description")
+      stub_get("/project/EX?expand=description", "project")
+      Jira.project("EX", expand: "description")
 
-      expect(a_get("/project/TEST?expand=description")).to have_been_made
+      expect(a_get("/project/EX?expand=description")).to have_been_made
     end
   end
 
@@ -43,6 +44,79 @@ RSpec.describe Jira::Client do
       Jira.archive_project("TEST")
 
       expect(a_post("/project/TEST/archive")).to have_been_made
+    end
+  end
+
+  describe ".update_project" do
+    it "updates a project", :aggregate_failures do
+      stub_put("/project/EX", "project")
+
+      project = Jira.update_project("EX", name: "New Name")
+
+      expect(a_put("/project/EX")).to have_been_made
+      expect(project[:key]).to eq("EX")
+      expect(project[:name]).to eq("Example")
+      expect(project.dig(:lead, :displayName)).to eq("Mia Krystof")
+    end
+  end
+
+  describe ".delete_project" do
+    it "deletes a project" do
+      stub_delete("/project/TEST", "empty")
+
+      Jira.delete_project("TEST")
+
+      expect(a_delete("/project/TEST")).to have_been_made
+    end
+  end
+
+  describe ".project_statuses" do
+    it "returns issue types with statuses", :aggregate_failures do
+      stub_get("/project/TEST/statuses", "project_statuses")
+
+      statuses = Jira.project_statuses("TEST")
+
+      expect(a_get("/project/TEST/statuses")).to have_been_made
+      expect(statuses).to be_an(Array)
+      expect(statuses.first[:name]).to eq("Task")
+      expect(statuses.first[:id]).to eq("3")
+      expect(statuses.first[:statuses].first[:name]).to eq("In Progress")
+    end
+  end
+
+  describe ".project_issue_type_hierarchy" do
+    it "returns the issue type hierarchy", :aggregate_failures do
+      stub_get("/project/10030/hierarchy", "project_issue_type_hierarchy")
+
+      hierarchy = Jira.project_issue_type_hierarchy(10_030)
+
+      expect(a_get("/project/10030/hierarchy")).to have_been_made
+      expect(hierarchy[:projectId]).to eq(10_030)
+      expect(hierarchy[:hierarchy]).to be_an(Array)
+      expect(hierarchy[:hierarchy].first[:name]).to eq("Epic")
+      expect(hierarchy[:hierarchy].first[:level]).to eq(1)
+    end
+  end
+
+  describe ".project_notification_scheme" do
+    it "returns the notification scheme", :aggregate_failures do
+      stub_get("/project/TEST/notificationscheme", "project_notification_scheme")
+
+      scheme = Jira.project_notification_scheme("TEST")
+
+      expect(a_get("/project/TEST/notificationscheme")).to have_been_made
+      expect(scheme[:id]).to eq(10_100)
+      expect(scheme[:name]).to eq("notification scheme name")
+      expect(scheme[:notificationSchemeEvents]).to be_an(Array)
+      expect(scheme[:notificationSchemeEvents].first.dig(:event, :name)).to eq("Issue created")
+    end
+
+    it "passes query options" do
+      stub_get("/project/TEST/notificationscheme?expand=all", "project_notification_scheme")
+
+      Jira.project_notification_scheme("TEST", expand: "all")
+
+      expect(a_get("/project/TEST/notificationscheme?expand=all")).to have_been_made
     end
   end
 end
