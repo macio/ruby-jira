b6e681eae8f769d23ecc57c47338e5ee642c89b9
Maciej Kozak <maciej.kozak@gmail.com>
2026-03-10 17:15:07 +0100

    feat: add update, delete, statuses, hierarchy, notification scheme project methods

--- lib/jira/client/projects.rb
+++ lib/jira/client/projects.rb
@@ -6,6 +6,8 @@ module Jira
     module Projects
       # Search projects
       #
+      # @url https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-search-get
+      #
       # @param options [Hash] Query parameters
       # @return [Jira::Request::PaginatedResponse]
       def projects(options = {})
@@ -13,6 +15,8 @@ module Jira
       end
 
       # Gets a single project
+      #
+      # @url https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectidorkey-get
       #
       # @param project_id_or_key [Integer, String] Project ID or key
       # @param options [Hash] Query parameters
@@ -23,10 +27,64 @@ module Jira
 
       # Archives a project
       #
+      # https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectidorkey-archive-post
+      #
       # @param project_id_or_key [Integer, String] Project ID or key
       # @return [Hash]
       def archive_project(project_id_or_key)
         post("/project/#{url_encode(project_id_or_key)}/archive")
+      end
+
+      # Updates a project
+      #
+      # @url https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectidorkey-put
+      #
+      # @param project_id_or_key [Integer, String] Project ID or key
+      # @param payload [Hash] Fields to update
+      # @return [Hash]
+      def update_project(project_id_or_key, payload = {})
+        put("/project/#{url_encode(project_id_or_key)}", body: payload)
+      end
+
+      # Deletes a project
+      #
+      # @url https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectidorkey-delete
+      #
+      # @param project_id_or_key [Integer, String] Project ID or key
+      # @return [Hash]
+      def delete_project(project_id_or_key)
+        delete("/project/#{url_encode(project_id_or_key)}")
+      end
+
+      # Gets all issue types with their statuses for a project
+      #
+      # @url https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectidorkey-statuses-get
+      #
+      # @param project_id_or_key [Integer, String] Project ID or key
+      # @return [Array<Hash>]
+      def project_statuses(project_id_or_key)
+        get("/project/#{url_encode(project_id_or_key)}/statuses")
+      end
+
+      # Gets the issue type hierarchy for a project
+      #
+      # @url https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectid-hierarchy-get
+      #
+      # @param project_id [Integer, String] Project ID
+      # @return [Hash]
+      def project_issue_type_hierarchy(project_id)
+        get("/project/#{url_encode(project_id)}/hierarchy")
+      end
+
+      # Gets the notification scheme for a project
+      #
+      # @url https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-projects/#api-rest-api-3-project-projectkeyorid-notificationscheme-get
+      #
+      # @param project_key_or_id [Integer, String] Project key or ID
+      # @param options [Hash] Query parameters (e.g. expand:)
+      # @return [Hash]
+      def project_notification_scheme(project_key_or_id, options = {})
+        get("/project/#{url_encode(project_key_or_id)}/notificationscheme", query: options)
       end
     end
   end
--- spec/jira/client/projects_spec.rb
+++ spec/jira/client/projects_spec.rb
@@ -45,4 +45,76 @@ RSpec.describe Jira::Client do
       expect(a_post("/project/TEST/archive")).to have_been_made
     end
   end
+
+  describe ".update_project" do
+    it "updates a project", :aggregate_failures do
+      stub_put("/project/TEST", "project")
+
+      project = Jira.update_project("TEST", name: "New Name")
+
+      expect(a_put("/project/TEST")).to have_been_made
+      expect(project[:key]).to eq("TEST")
+      expect(project.dig(:lead, :displayName)).to eq("Test User")
+    end
+  end
+
+  describe ".delete_project" do
+    it "deletes a project" do
+      stub_delete("/project/TEST", "empty")
+
+      Jira.delete_project("TEST")
+
+      expect(a_delete("/project/TEST")).to have_been_made
+    end
+  end
+
+  describe ".project_statuses" do
+    it "returns issue types with statuses", :aggregate_failures do
+      stub_get("/project/TEST/statuses", "project_statuses")
+
+      statuses = Jira.project_statuses("TEST")
+
+      expect(a_get("/project/TEST/statuses")).to have_been_made
+      expect(statuses).to be_an(Array)
+      expect(statuses.first[:name]).to eq("Task")
+      expect(statuses.first[:id]).to eq("3")
+      expect(statuses.first[:statuses].first[:name]).to eq("In Progress")
+    end
+  end
+
+  describe ".project_issue_type_hierarchy" do
+    it "returns the issue type hierarchy", :aggregate_failures do
+      stub_get("/project/10030/hierarchy", "project_issue_type_hierarchy")
+
+      hierarchy = Jira.project_issue_type_hierarchy(10_030)
+
+      expect(a_get("/project/10030/hierarchy")).to have_been_made
+      expect(hierarchy[:projectId]).to eq(10_030)
+      expect(hierarchy[:hierarchy]).to be_an(Array)
+      expect(hierarchy[:hierarchy].first[:name]).to eq("Epic")
+      expect(hierarchy[:hierarchy].first[:level]).to eq(1)
+    end
+  end
+
+  describe ".project_notification_scheme" do
+    it "returns the notification scheme", :aggregate_failures do
+      stub_get("/project/TEST/notificationscheme", "project_notification_scheme")
+
+      scheme = Jira.project_notification_scheme("TEST")
+
+      expect(a_get("/project/TEST/notificationscheme")).to have_been_made
+      expect(scheme[:id]).to eq(10_100)
+      expect(scheme[:name]).to eq("notification scheme name")
+      expect(scheme[:notificationSchemeEvents]).to be_an(Array)
+      expect(scheme[:notificationSchemeEvents].first.dig(:event, :name)).to eq("Issue created")
+    end
+
+    it "passes query options" do
+      stub_get("/project/TEST/notificationscheme?expand=all", "project_notification_scheme")
+
+      Jira.project_notification_scheme("TEST", expand: "all")
+
+      expect(a_get("/project/TEST/notificationscheme?expand=all")).to have_been_made
+    end
+  end
 end
